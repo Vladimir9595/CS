c72b9eadc1f1c935e5b7239e687c89db7de0ed2f
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-09-29 18:31:01 +0200

    fix: all functions run correctly

--- TP0_SACCHETTO_Vladimir/TP0_SACCHETTO_Vladimir/Program.cs
+++ TP0_SACCHETTO_Vladimir/TP0_SACCHETTO_Vladimir/Program.cs
@@ -6,142 +6,19 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Policy;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TP0_SACCHETTO_Vladimir
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            // Welcome message
-            Console.WriteLine("Bienvenue sur mon programme, jeune étranger imberbe. Presse entrer pour y rentrer ...");
-            Console.ReadLine();
 
-            string surname;
-            bool isValidSurname;
-            string name;
-            bool isValidName;
+            RunProgram();   
+            Menu();
 
-            // Vérification du nom de famille
-            do
-            {
-                Console.WriteLine("Donne moi ton nom de famille, vil chenapan : ");
-                surname = Console.ReadLine();
-
-                isValidSurname = !string.IsNullOrWhiteSpace(surname) && !ContainsNumbers(surname);
-
-                if (!isValidSurname)
-                {
-                    Console.WriteLine("Erreur : Veuilles entrer un nom de famille valide (sans chiffres ni espaces vides).");
-                    Console.WriteLine();
-                }
-            } while (!isValidSurname);
-
-            Console.WriteLine("Nom de famille valide : " + surname);
-            Console.WriteLine();
-            
-            // Vérification du prenom
-            do
-            {
-                Console.WriteLine("Et quel est ton prénom, petit galopin : ");
-                name = Console.ReadLine();
-
-                isValidName = !string.IsNullOrWhiteSpace(name) && !ContainsNumbers(name);
-
-                if (!isValidName)
-                {
-                    Console.WriteLine("Erreur : Veuilles entrer un prénom valide (sans chiffres ni espaces vides).");
-                    Console.WriteLine();
-                }
-            } while (!isValidName);
-
-            Console.WriteLine("Ton prenom est aussi valide : " + name);
-            Console.WriteLine();
-
-            
-            // Affichage du nom et du prénom avec fonction
-            Console.WriteLine(NameFormat(name, surname));
-            Console.ReadLine();
-
-
-            // Affichage de la taille, du poids et de l'âge
-            int height = RetrieveHeight();
-            int weight = RetrieveWeight();
-            int age = RetrieveAge();
-
-            // Affichage de la majorité
-
-            Console.WriteLine(age <= 18
-            ? "Tu es un morveux puéril et immature qui n’a même pas le droit d’acheter de l’alcool en grande surface."
-            : "Tu es majeur ! Tu es prêt.e à consommer ta première dose d’alcool !");
-            Console.ReadLine();
-
-            // IMC
-            Console.WriteLine(ToString(height, weight));
-            Console.ReadLine();
-
-            // Commentaire IMC
-            CommentToDisplay(height, weight);
-            Console.ReadLine();
-
-             
-            // Estimation du nombre de cheveux avec do ... while et if ... else
-            Console.WriteLine("-------------------------------------------------------------------------");
-            Console.WriteLine("Maintenant nous allons estimer le nombre de cheveux que tu as sur ta tête");
-            Console.WriteLine("-------------------------------------------------------------------------");
-            Console.ReadLine();
-            Estimation();
-
-
-            
-            
-            // Jeu du choix
-            int choice;
-            bool isValidChoice;
-
-            Console.WriteLine("------------------------------------------------------------------");
-            Console.WriteLine("Enfin nous allons terminer avec le jeu du choix !");
-            Console.WriteLine("------------------------------------------------------------------");
-            Console.ReadLine();
-
-            do
-            {
-                Menu();
-                string input = Console.ReadLine();
-
-                isValidChoice = int.TryParse(input, out choice);
-
-                // isValidChoice = !string.IsNullOrWhiteSpace(choice);
-
-                if (isValidChoice == true)
-                {
-                    Console.WriteLine("Erreur : Veuillez entrer un choix valide.");
-                    Console.WriteLine();
-                }
-            } while (isValidChoice != true);   
-
-
-            // TimeOut de 5 secondes
-            await TimeOut();
-
-        }
-
-        public static async Task TimeOut() 
-        { 
-            var inputTask = Task.Run(() => Console.ReadLine());
-
-            var completedTask = await Task.WhenAny(inputTask, Task.Delay(TimeSpan.FromSeconds(5))); // Temps limite de 10 secondes
-
-            if (completedTask == inputTask)
-            {
-                string input = inputTask.Result;
-                Console.WriteLine("Vous avez entré : " + input);
-            }
-            else
-            {
-                Console.WriteLine("Le temps imparti est écoulé. Le programme se ferme.");
-            }
         }
         
         public static string NameFormat(string name, string surname)
@@ -166,17 +43,21 @@ namespace TP0_SACCHETTO_Vladimir
 
                 if (isValidHeight && height > 0)
                 {
+                    Console.WriteLine();
                     Console.WriteLine("Ta taille est valide : " + height + "cm.");
-                    Console.ReadLine();
+                    Console.WriteLine();
                 }
                 else
                 {
+                    Console.WriteLine();
                     Console.WriteLine("Erreur : Entres une taille valide (j'accepte les décimaux).");
                     Console.WriteLine();
                 }
 
-            } while (!isValidHeight && !(height >= 0));
+            } while (!isValidHeight || height <= 0);
+
             
+
             return height;
         
         }
@@ -198,16 +79,18 @@ namespace TP0_SACCHETTO_Vladimir
 
                 if (isValidWeight && weight >= 0)
                 {
+                    Console.WriteLine();
                     Console.WriteLine("Ton poids est valide : " + weight + "kg.");
-                    Console.ReadLine();
+                    Console.WriteLine();
                 }
                 else
                 {
+                    Console.WriteLine();
                     Console.WriteLine("Erreur : Entres un poids valide (j'accepte les décimaux).");
                     Console.WriteLine();
                 }
 
-            } while (!isValidWeight && !(weight >= 0));
+            } while (!isValidWeight || weight <= 0);
 
             return weight;
         }
@@ -231,15 +114,17 @@ namespace TP0_SACCHETTO_Vladimir
 
                 if (isValidAge && age >= 0)
                 {
+                    Console.WriteLine();
                     Console.WriteLine("Ton age est valide : " + age + "ans.");
-                    Console.ReadLine();
+                    Console.WriteLine();
                 }
                 else
                 {
+                    Console.WriteLine();
                     Console.WriteLine("Erreur : Entres un vrai âge.");
                     Console.WriteLine();
                 }
-            } while (!isValidAge && !(age >= 0));
+            } while (!isValidAge || age <= 0);
 
             return age;
         }
@@ -285,16 +170,16 @@ namespace TP0_SACCHETTO_Vladimir
 
                 if (isValidEstimate && (estimate >= 100000 && estimate <= 150000))
                 {
-                    Console.WriteLine("Estimation valide : " + estimate);
+                    Console.WriteLine("\nEstimation valide : " + estimate);
                     Console.ReadLine();
                 }
                 else
                 {
-                    Console.WriteLine("Erreur : Veuillez entrer un nombre valide (entre 100 000 et 150 000).");
+                    Console.WriteLine("\nErreur : Veuillez entrer un nombre valide (entre 100 000 et 150 000).");
                     Console.WriteLine();
                 }
 
-            } while (isValidEstimate && !(estimate >= 100000 && estimate <= 150000));
+            } while (!isValidEstimate || estimate <= 100000 && estimate >= 150000);
 
             Console.WriteLine("Merci pour ton estimation. Rassure toi, tu as encore des cheveux !");
             Console.ReadLine();
@@ -346,15 +231,162 @@ namespace TP0_SACCHETTO_Vladimir
 
         public static void Menu()
         {
-            Console.WriteLine("\t Tu vas choisir entre ces 4 possibilités : ");
+            bool sleepProgram = false;
+
+            while (!sleepProgram)
+            { 
+
+                Console.WriteLine("\t Tu vas choisir entre ces 4 possibilités : ");
+                Console.WriteLine();
+                Console.WriteLine("1 - Tu souhaites quitter mon programme .......... ");
+                Console.WriteLine("2 - Tu souhaites recommencer le programme =) ");
+                Console.WriteLine("3 - Tu souhaites compter jusqu’à 10 ");
+                Console.WriteLine("4 - Tu souhaites téléphoner à Tata Jacqueline ");
+                Console.WriteLine();
+                Console.Write("Fais ton choix : ");
+                string choise = Console.ReadLine();
+
+                switch (choise)
+                {
+                    case "1" :
+                        Console.WriteLine();
+                        Console.WriteLine("Tu as choisi de quitter mon programme. Au revoir !");
+                        sleepProgram = true;
+                        Sleep();
+                        break;
+                    case "2" :
+                        Console.WriteLine();
+                        Console.WriteLine("Tu as choisi de recommencer le programme. C'est reparti !");
+                        Console.WriteLine("****************************************************************************************");
+                        Console.WriteLine();
+                        Thread.Sleep(1000);
+                        RunProgram();
+                        break;
+                    case "3" :
+                        Console.WriteLine();
+                        Console.WriteLine("Tu as choisi de compter jusqu'à 10. C'est parti !");
+                        for (int i = 1; i <= 10; i++)
+                        {
+                            Console.WriteLine(i);
+                            Thread.Sleep(1000);
+                        }
+                        Console.WriteLine("Merci d'avoir compté avec moi. Au revoir !");
+                        sleepProgram = true;
+                        Sleep();
+                        break;
+                    case "4" :
+                        Console.WriteLine();
+                        Console.WriteLine("Tu as choisi de téléphoner à Tata Jacqueline. C'est parti !");
+                        Console.WriteLine();
+                        Thread.Sleep(1000);
+                        Console.WriteLine("Messagerie de Tata Jacqueline : Bonjour je suis actuellement absente mais en général je n'aime pas que les gens me parlent alors ne m'appelle plus jamais !!!! ");
+                        Thread.Sleep(5000);
+                        sleepProgram = true;
+                        Sleep();
+                        break;
+
+                    default:
+                        Console.WriteLine("Choix invalide. Veuillez entrer un numéro entre 1 et 4.");
+                        break;
+                }
+        
+            }
+        }
+
+        public static void Sleep() 
+        { 
+            Thread.Sleep(3000);
+            return;
+        }
+
+        public static void RunProgram()
+        {
+            // Welcome message
+            Console.WriteLine("Bienvenue sur mon programme, jeune étranger imberbe. Presse entrer pour y rentrer ...");
+            Console.ReadLine();
+
+            string surname;
+            bool isValidSurname;
+            string name;
+            bool isValidName;
+
+            // Vérification du nom de famille
+            do
+            {
+                Console.Write("Donne moi ton nom de famille, vil chenapan : ");
+                surname = Console.ReadLine();
+
+                isValidSurname = !string.IsNullOrWhiteSpace(surname) && !ContainsNumbers(surname);
+
+                if (!isValidSurname)
+                {
+                    Console.WriteLine("\nErreur : Veuilles entrer un nom de famille valide (sans chiffres ni espaces vides).");
+                    Console.WriteLine();
+                }
+            } while (!isValidSurname);
+
+            Console.WriteLine("\nNom de famille valide : " + surname);
             Console.WriteLine();
-            Console.WriteLine("1 - Tu souhaites quitter mon programme .......... ");
-            Console.WriteLine("2 - Tu souhaites recommencer le programme =) ");
-            Console.WriteLine("3 - Tu souhaites compter jusqu’à 10 ");
-            Console.WriteLine("4 - Tu souhaites téléphoner à Tata Jacqueline ");
+
+            // Vérification du prenom
+            do
+            {
+                Console.Write("\nEt quel est ton prénom, petit galopin : ");
+                name = Console.ReadLine();
+
+                isValidName = !string.IsNullOrWhiteSpace(name) && !ContainsNumbers(name);
+
+                if (!isValidName)
+                {
+                    Console.WriteLine("\nErreur : Veuilles entrer un prénom valide (sans chiffres ni espaces vides).");
+                    Console.WriteLine();
+                }
+            } while (!isValidName);
+
+            Console.WriteLine("\nTon prenom est aussi valide : " + name);
             Console.WriteLine();
-            Console.WriteLine("\t\n Fais ton choix : ");
+
+
+            // Affichage du nom et du prénom avec fonction
+            Console.WriteLine(NameFormat(name, surname));
+            Console.ReadLine();
+
+
+            // Affichage de la taille, du poids et de l'âge
+            int height = RetrieveHeight();
+            int weight = RetrieveWeight();
+            int age = RetrieveAge();
+
+            // Affichage de la majorité
+
+            Console.WriteLine(age <= 18
+            ? "Tu es un morveux puéril et immature qui n’a même pas le droit d’acheter de l’alcool en grande surface."
+            : "Tu es majeur ! Tu es prêt.e à consommer ta première dose d’alcool !");
+            Console.ReadLine();
+
+            // IMC
+            Console.WriteLine(ToString(height, weight));
+            Console.ReadLine();
+
+            // Commentaire IMC
+            CommentToDisplay(height, weight);
+            Console.ReadLine();
+
+
+            // Estimation du nombre de cheveux avec do ... while et if ... else
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("Maintenant nous allons estimer le nombre de cheveux que tu as sur ta tête");
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.ReadLine();
+            Estimation();
+
+            // Jeu du choix
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Enfin nous allons terminer avec le jeu du choix !");
+            Console.WriteLine("-------------------------------------------------");
+            Console.ReadLine();
+
         }
     }
-        
+
 }
