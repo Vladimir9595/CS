521e67a31d119a4f2ba100832228d35c6111e373
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-11-20 14:34:41 +0100

    feat: add interface spaceship

---
+++ SACCHETTO_Vladimir_TP1/SACCHETTO_Vladimir_TP1/ISpaceship.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SACCHETTO_Vladimir_SpaceInvaders.Spaceship;
+
+namespace SACCHETTO_Vladimir_SpaceInvaders
+{
+    public interface ISpaceship
+    {
+        string Name { get; set; }
+        double Structure { get; set; }
+        double Shield { get; set; }
+        bool IsDestroyed { get; }
+        int MaxWeapons { get; }
+        List<Weapon> Weapons { get; }
+        double AverageDamages { get; }
+        double CurrentStructure { get; set; }
+        double CurrentShield { get; set; }
+        bool BelongsPlayer { get; }
+        void TakeDamages(double damages);
+        double GetAverageDamages();
+        void RepairShield(double repair);
+        void RepairAmount(int repairAmount);
+        void ShootTarget(Spaceship target);
+        void ReloadWeapons();
+        void AddWeapon(Weapon weapon);
+        void RemoveWeapon(Weapon oWeapon);
+        void ClearWeapons();
+        void ViewShip();
+        void ViewWeapons();
+        
+
+    }
+}
