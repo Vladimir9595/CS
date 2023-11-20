df0e51d0df1ed9fc186ac9375a58df18ee23b701
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-11-20 19:04:11 +0100

    feat : add IWeapon interface

---
+++ SACCHETTO_Vladimir_SpaceInvaders/SACCHETTO_Vladimir_SpaceInvaders/IWeapon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SACCHETTO_Vladimir_SpaceInvaders
+{
+    public interface IWeapon
+    {
+        string Name { get; set; }
+        EWeaponType WeaponType { get; set; }
+        double MinDamage { get; set; }
+        double MaxDamage { get; set; }
+        double AverageDamage { get; }
+        double ReloadTime { get; set; }
+        double TimeBeforeReload { get; set; }
+        bool IsReload { get; }
+        double Reload();
+        double Shoot();
+    }
+}
