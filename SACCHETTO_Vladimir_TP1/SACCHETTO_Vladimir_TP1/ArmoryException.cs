6ae6d52f7badefea51d64aa9fe6af746b2b34518
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-11-20 14:37:30 +0100

    feat: change namespace

--- SACCHETTO_Vladimir_TP1/SACCHETTO_Vladimir_TP1/ArmoryException.cs
+++ SACCHETTO_Vladimir_TP1/SACCHETTO_Vladimir_TP1/ArmoryException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SACCHETTO_Vladimir_TP1
+namespace SACCHETTO_Vladimir_SpaceInvaders
 {
     public class ArmoryException : Exception
     {
