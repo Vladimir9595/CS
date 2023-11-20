2c1075a227dbe9b40f9bce1290218c0879448681
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-11-20 19:03:40 +0100

    feat : add IAbility interface

---
+++ SACCHETTO_Vladimir_SpaceInvaders/SACCHETTO_Vladimir_SpaceInvaders/IAbility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SACCHETTO_Vladimir_SpaceInvaders
+{
+    public interface IAbility
+    {
+        void UseAbility(List<Spaceship> spaceships);
+    }
+
+}
