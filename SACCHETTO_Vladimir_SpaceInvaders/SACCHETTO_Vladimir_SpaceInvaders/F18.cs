b090cc9f6034b6684c60eb18c0200dde3476b0cc
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-11-20 19:03:03 +0100

    feat : add F18 class

---
+++ SACCHETTO_Vladimir_SpaceInvaders/SACCHETTO_Vladimir_SpaceInvaders/F18.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SACCHETTO_Vladimir_SpaceInvaders
+{
+    public class F18 : Spaceship, IAbility
+    {
+        public F18() : base("F18", structure: 15, shield: 0, currentStructure: 15, maxStructure: 100, currentShield: 0, maxShield: 30)
+        {
+        }
+
+        public void UseAbility(List<Spaceship> spaceships)
+        {
+            Console.WriteLine($"{Name} is using its special ability!");
+            ExplodeOnContact(spaceships);
+        }
+
+        private void ExplodeOnContact(List<Spaceship> spaceships)
+        {
+     
+            if (spaceships.IndexOf(this) == 0 || spaceships.IndexOf(this) == 2)
+            {
+                Console.WriteLine($"{Name} is in position 0 or 2 and is exploding!");
+
+   
+                foreach (var spaceship in spaceships)
+                {
+                    if (spaceship != this)
+                    {
+                        spaceship.TakeDamages(10);
+                    }
+                }
+            }
+        }
+
+        public override void ShootTarget(Spaceship target)
+        {
+            Console.WriteLine($"{Name} does not have weapons and cannot shoot.");
+        }
+
+        public override void TakeDamages(double damages)
+        {
+            // Le F-18 n'est pas affecté par les dégâts entrants, car il n'a pas de bouclier ni de structure
+            // Le F-18 inflige des dégâts lorsqu'il explose sur un vaisseau voisin (gestion dans la méthode ExplodeOnContact)
+        }
+    }
+}
