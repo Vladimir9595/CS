b6934fb5f145b2e9671143dc61bbf2373e393ad1
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-11-20 19:03:27 +0100

    feat : add IPlayer interface

---
+++ SACCHETTO_Vladimir_SpaceInvaders/SACCHETTO_Vladimir_SpaceInvaders/IPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SACCHETTO_Vladimir_SpaceInvaders.Spaceship;
+
+namespace SACCHETTO_Vladimir_SpaceInvaders
+{
+    public interface IPlayer
+    {
+        Spaceship BattleShip { get; set; }
+        string PlayerName { get; }
+        string PlayerAlias { get; }
+    }
+}
